0b263352437e7d202ad64df8fb36d801d17681aa
JosueLC <josueflopez@msn.com>
2022-06-23 20:40:15 -0500

    maquetado de la app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import './App.css';
+ import React from 'react';
+
+ import {ToDoCounter} from './ToDoCounter';
+ import {ToDoSearch} from './ToDoSearch';
+ import {ToDoList} from './ToDoList';
+ import {ToDoItem} from './ToDoItem';
+ import {CreateToDoButton} from './CreateToDoButton';
+
+// import './App.css';
+
+const todos = [
+  {text:"Curso Intro React"},
+  {text:"Curso Advance React"},
+  {text:"Portfolio with React"},
+  {text:"SIF with React"}
+];
 
 function App() {
   return (
     <React.Fragment>
-      <h1>Hola, soy Josue.</h1>
+    <ToDoCounter />
+    <ToDoSearch />
+    <ToDoList>
+        {todos.map(todo => (
+          <ToDoItem key={todos.indexOf(todo)} text={todo.text}/>
+        ))}
+      </ToDoList>
+      <CreateToDoButton />
     </React.Fragment>
   );
 }
---
+++ src/ToDoCounter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ToDoCounter(){
+    return (
+        <h2>Has completado X de Y TODOs</h2>
+    );
+}
+
+export {ToDoCounter};
