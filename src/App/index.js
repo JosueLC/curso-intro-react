49bb74ee8bf83dfa8b8c7f39fdbf782f333feb59
JosueLC <josueflopez@msn.com>
2022-07-20 13:37:16 -0500

    removed useContext

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,57 @@
- import React from 'react';
- import { AppUI } from './AppUI';
-
- import { ToDoProvider} from '../ToDoContext';
+import React from 'react';
+import { useTodos } from './useTodos';
+import { ToDoHeader } from '../ToDoHeader';
+import { ToDoCounter } from '../ToDoCounter';
+import { ToDoSearch } from '../ToDoSearch';
+import { ToDoList } from '../ToDoList';
+import { ToDoItem } from '../ToDoItem';
+import { CreateToDoButton} from '../CreateToDoButton';
+import { Modal } from '../Modal';
+import { ToDoForm } from '../ToDoForm';
+import { ToDoContentLoader } from '../ToDoContentLoader';
+import './App.css';
 
 function App() {
+  const value = useTodos();
   return (
-    <ToDoProvider>
-      <AppUI />
-    </ToDoProvider>
+      <React.Fragment>
+        <ToDoHeader>
+          <ToDoCounter
+            totalTodos={value.totalTodos}
+            completedTodos={value.completedTodos}
+          />
+          <ToDoSearch
+            searchValue={value.searchValue}
+            setSearchValue={value.setSearchValue}
+          />
+        </ToDoHeader>
+        <ToDoList>
+            {value.errorState && <p>Hubo un error: {value.errorState.message}</p>}
+            {value.loadingState && <ToDoContentLoader/> }
+            {(!value.loadingState && !value.filteredTodos.length) && <p>Crea tu primer ToDo.</p>}
+            {value.filteredTodos.map(todo => (
+            <ToDoItem
+                key={value.filteredTodos.indexOf(todo)}
+                text={todo.text}
+                completed={todo.completed}
+                onComplete={()=>{value.toggleCompleteToDo(todo.text)}}
+                onDelete={()=>{value.deleteToDo(todo.text)}}
+            />
+            ))}
+        </ToDoList>
+        {value.modalState && (
+            <Modal>
+                <ToDoForm
+                    addToDo={value.addToDo}
+                    setModalState={value.setModalState}
+                />
+            </Modal>
+        )}
+        <CreateToDoButton
+            modalState={value.modalState}
+            setModalState={value.setModalState}
+        />
+      </React.Fragment>
   );
 }
 
